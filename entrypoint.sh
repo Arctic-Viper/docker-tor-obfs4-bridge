471cce299e3e3104599000ac28cbd36b35f781bd
Arc <48611080+Arctic-Viper@users.noreply.github.com>
2023-11-01 21:31:26 -0400

    Added more variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,9 +12,10 @@ sed -e "s#{OR_PORT}#$OR_PORT#" \
     -e "s#{CONTACT_INFO}#$CONTACT_INFO#" \
     -e "s#{NICKNAME}#$NICKNAME#" \
     -e "s#{ACCOUNTINGMAX}#$ACCOUNTINGMAX" \
+    -e "s#{ACCOUNTINGSTART}#$ACCOUNTINGSTART" \
     -e "s#{BANDWIDTHRATE}#$BANDWIDTHRATE#" \
     -e "s#{BANDWIDTHBURST}#$BANDWIDTHBURST#" \
-    -e "s#{NOEXEC}#$NOEXEC#" \
+    -e "s#{EXITRELAY}#$EXITRELAY#" \
     -e "s#{DISABLE_IPV6}#$DISABLE_IPV6#" \
     /torrc.template >/tmp/torrc
 
